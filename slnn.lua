cea2bdd5b538daeb6b63e8dc8ddd05d41e9fa1e3
TheWaffleDimension <thewaffledimension@gmail.com>
2018-11-13 13:11:01 -0600

    Began train function

--- slnn.lua
+++ slnn.lua
@@ -1,4 +1,4 @@
-local mx = require 'game/matrix/matrix'
+local mx = require"matrix/matrix"
 math.randomseed(os.time())
 local Library = {}
 
@@ -17,10 +17,37 @@ local function Forward(model, input, activationFunc)
     for i = 1, #model.Layers, 1 do
         result = mx.replace(mx.mul(result, model.hiddenLayers[i]), activationFunc)
     end
-    
+
     return result
 end
-----------------------------------------------------------------------
+-----------------------
+local function Train(model, gens, trainData, genDoneFunc)
+  for gen=1, gens, 1 do
+    local errors = {}
+    for i=1, #trainData, 1 do
+      local input = trainData[i][1]
+      local expectedOutputMx = trainData[i][2]
+      local actualOutputMx = model:Forward(input)
+
+      local expectedOutput = 0
+      for i,v in mx.ipairs(expectedOutputMx) do
+        expectedOutput = expectedOutput + v
+      end
+
+      local actualOutput = 0
+      for i,v in mx.ipairs(actualOutputMx) do
+        actualOutput = actualOutput + v
+      end
+
+      local error = expectedOutput - actualOutput
+      table.insert(errors, error)
+    end
+
+    table.sort(errors)
+
+    print(string.format("[Generation %d:] Error: %d", gen, errors[1]))
+  end
+end-----------------------------------------------
 
 -- layers : Table {inputs, ...hiddens..., outputs}
 function Library.CreateModel(layers, activationFunc)
@@ -28,15 +55,15 @@ function Library.CreateModel(layers, activationFunc)
     layers = layers or {1, 1}
 
     local model = {LayerCount = layers, ActivationFunction = activationFunc, Layers = {}}
-    
+
     for i = math.min(#layers, 2), #layers, 1 do
         local layer = mx(layers[i-1], layers[i])
         mx.random(layer)
         table.insert(model.Layers, layer)
     end
-    
+
     model.Forward = Forward
-    
+
     return model
 end
 
--- util.lua
+++ util.lua
@@ -1,9 +1,19 @@
+local mx = require"matrix/matrix"
+
 local UtilityFunctions = {}
 
 -- Flat a number to 0 or 1
 UtilityFunctions.flat = function(x)
     if x < 0.5 then return 0
     else return 1 end
+end
+
+UtilityFunctions.randomizeLayers = function(model)
+  for i,v in next,model.Layers,nil do
+    mx.random(v)
+  end
+
+  return model
 end
 
 return UtilityFunctions
